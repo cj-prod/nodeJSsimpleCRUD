3c9e7b1f3c09afd5cff330bd089f3ddf0ee041dd
cj-prod <32711964+cj-prod@users.noreply.github.com>
2019-05-12 20:42:36 +0200

    third commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-	module.exports = {mongoURI: 'mongodb+srv://cjprod:cjprod142@tmcluster-9orb7.mongodb.net/test?retryWrites=true'}
+	module.exports = {mongoURI: 'mongodb+srv://cjprod:cjprod142@tm2cluster-9orb7.mongodb.net/test?retryWrites=true'}
 } else{
 	module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
